6d350141191d097aac73983837e4642e64d5a82c
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2017-03-07 16:13:03 +0530

    Rait_a_thon

---
+++ src/Rait_a_thon.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Created by anshu on 07/03/17.
+ */
+public class Rait_a_thon {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        BigInteger factorial = BigInteger.ONE;
+        for(int i=2;i<=n;i++){
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(factorial);
+    }
+}
